30859d897d48c131ccd426b04171204837e7d69a
Magyar Ádám <60811003+Adika713@users.noreply.github.com>
2023-10-19 13:12:03 +0200

    Utolsó módosítások

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
       const isMenuOpen = document.body.classList.toggle('menu-open');
 
       if (isMenuOpen) {
-        // Nyitott állapotban vagyunk, átirányítjuk az oldalt
         setTimeout(() => {
           window.location.href = "../dropdown-menu/ipad-menu-dropdown.html";
-        }, 300); // Az időpontot az átmenet időzítőjéhez kell igazítani
+        }, 300);
       }
     });
   });
@@ -20,10 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
       const isMenuOpen = document.body.classList.toggle('menu-open');
 
       if (isMenuOpen) {
-        // Nyitott állapotban vagyunk, átirányítjuk az oldalt
         setTimeout(() => {
           window.location.href = "../index.html";
-        }, 300); // Az időpontot az átmenet időzítőjéhez kell igazítani
+        }, 300);
       }
     });
   });
@@ -35,17 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
       const isMenuOpen = document.body.classList.toggle('menu-open');
 
       if (isMenuOpen) {
-        // Nyitott állapotban vagyunk, átirányítjuk az oldalt
         setTimeout(() => {
           window.location.href = "../dropdown-menu/ipad-menu-dropdown.html";
-        }, 300); // Az időpontot az átmenet időzítőjéhez kell igazítani
+        }, 300);
       }
     });
-  });
-
-
-
-  document.getElementById('playButton').addEventListener('click', function() {
-    var audio = new Audio('audio/himnusz.mp3');
-    audio.play();
   });
